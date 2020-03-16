6dc23768088a09ced34d34d910e92e03b3182f8e
Dafiul Haq <dafiulhaq97@gmail.com>
2020-03-16 16:14:27 +0700

    hapus tab di message

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ async function postToFacebook(){
     let {total, indonesia, affected, time} = await data();
     let msg = `Update Terbaru Virus Corona (COVID-19) di Indonesia
 
-    - Terinfeksi: ${indonesia.infections}
-    - Aktif: ${indonesia.active_cases}
-    - Kematian: ${indonesia.deaths}
-    - Sembuh: ${indonesia.recovered}
+- Terinfeksi: ${indonesia.infections}
+- Aktif: ${indonesia.active_cases}
+- Kematian: ${indonesia.deaths}
+- Sembuh: ${indonesia.recovered}
 
-    Jumlah keseluruhan korban di seluruh dunia
-    - Terinfeksi: ${total.infections}
-    - Aktif: ${total.active_cases}
-    - Kematian: ${total.deaths}
-    - Telah pulih: ${total.recovered}
-    - Tingkat kematian: ${total.mortality_rate}
-    - Tingkat pemulihan: ${total.recovery_rate}
-    - Jumlah terdampak: ${affected} negara
+Jumlah keseluruhan korban di seluruh dunia
+- Terinfeksi: ${total.infections}
+- Aktif: ${total.active_cases}
+- Kematian: ${total.deaths}
+- Telah pulih: ${total.recovered}
+- Tingkat kematian: ${total.mortality_rate}
+- Tingkat pemulihan: ${total.recovery_rate}
+- Jumlah terdampak: ${affected} negara
 
-    Data diperbarui pada ${time}
-    Info selengkapnya dapat diakses di http://thewuhanvirus.com`;
+Data diperbarui pada ${time}
+Info selengkapnya dapat diakses di http://thewuhanvirus.com`;
     let url = "https://graph.facebook.com/" + process.env.PAGE_ID + "/feed?access_token=" + process.env.ACCESS_TOKEN + "&message=" + encodeURI(msg);
     
     await axios.post(url);
